f58ea91804623513b1fac0f51110dda51ba67502
Orino1 <orinomohamed@gmail.com>
2023-09-27 21:02:31 +0100

    Adding 1-install_a_package.pp File

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,5 +1,14 @@
-# Inall puppet-lint
-package { 'puppet-lint':
-  ensure   => '2.5.0',
-  provider => 'gem'
+# Define a package resource for Flask
+package { 'python3-flask':
+  ensure   => '2.1.0',
+  provider => 'pip3',
 }
+
+# Notify a command to print Flask version
+exec { 'print_flask_version':
+  command     => '/usr/bin/pip3 show Flask | grep Version',
+  path        => ['/usr/bin', '/usr/local/bin'],
+  refreshonly => true,
+  notify      => Exec['print_flask_version'],
+}
+
