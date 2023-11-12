2441b0c73b9e6268f7dc74451cc94cd4df6b6de4
Orino1 <orinomohamed@gmail.com>
2023-11-12 02:15:43 +0100

    Update: no need for reloading the apache server

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,7 +1,7 @@
 # this manifests will fix the wrong extention in the file wp-settings.php to fix our webstack
 
 exec { 'execute_commands':
-  command => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php ; service apache2 reload',
+  command => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php',
   path    => 'usr/local/bin:/bin/'
 }
 
