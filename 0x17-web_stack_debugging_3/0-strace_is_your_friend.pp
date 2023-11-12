95c4a4cd29fc91e69780f0d7014eb1bbf1aa17f9
Orino1 <orinomohamed@gmail.com>
2023-11-12 02:11:58 +0100

    Update: puppet-lint compliance

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,7 +1,7 @@
 # this manifests will fix the wrong extention in the file wp-settings.php to fix our webstack
 
 exec { 'execute_commands':
-  command     => 'sed -i “s/phpp/php/g” /var/www/html/wp-settings.php ; service apache2 reload',
-  path        => 'usr/local/bin:/bin/'
+  command => 'sed -i “s/phpp/php/g” /var/www/html/wp-settings.php ; service apache2 reload',
+  path    => 'usr/local/bin:/bin/'
 }
 
